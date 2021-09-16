dea9b850cea9e9a5f7f91d873e0aeb0198719d14
christian.abraham <christian.abraham@email.cz>
2021-09-16 14:51:42 +0200

    lll

--- programujemstillem/src/com/company/Main.java
+++ programujemstillem/src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        /*naucim se bruh*/
         Scanner sc = new Scanner(System.in);
         System.out.println("Zadej cislo");
         int a = sc.nextInt();
